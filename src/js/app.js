7a721442d87ebe58083048b5496a36417bd69029
AslanSN <alan.staub@hotmail.com>
2021-11-15 14:29:48 +0000

    Sustitution of default nulls

--- src/js/app.js
+++ src/js/app.js
@@ -3,39 +3,56 @@ import "../style/index.css";
 /**
  *  EDIT ONLY INSIDE THIS RENDER FUNCTION
  *  This function is called every time the user changes types or changes any input
- * 
-    {
-        includeCover: true, // if includeCover is true the algorithm should
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the url of the image that will used as background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
-        socialMediaPosition: "left", // social media bar position (left or right)
-        
-        twitter: null, // social media usernames
-        github: null,
-        linkedin: null,
-        instagram: null,
+ * */
+// {
+//     includeCover: true, // if includeCover is true the algorithm should
+//     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the url of the image that will used as background for the profile cover
+//     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
+//     socialMediaPosition: "left", // social media bar position (left or right)
 
-        name: null,
-        lastname: null,
-        role: null,
-        country: null,
-        city: null
-    }
- */
+//     twitter: null, // social media usernames
+//     github: null,
+//     linkedin: null,
+//     instagram: null,
+
+//     name: null,
+//     lastname: null,
+//     role: null,
+//     country: null,
+//     city: null
+// }
+
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); //print on the console
+  console.log("These are the current variables: ", variables);
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
+
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
+
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+
+          <h1>${variables.name != null ? variables.name : "Type your name "}
+          ${
+            variables.lastname != null
+              ? variables.lastname
+              : "and your surname."
+          }</h1>
+
+          <h2>${
+            variables.role != null ? variables.role : "Your role is...?"
+          }</h2>
+          
+          <h3>${
+            variables.country != null
+              ? variables.country
+              : "Where are you from?"
+          }</h3>
+
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
